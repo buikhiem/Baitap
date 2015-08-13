8b984f4bf301215d2da3bb4b23f3ffba08a0d348
buikhiem <buikhiem1994@gmail.com>
2015-08-13 12:03:16 +0700

    file moi

--- email_activation/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$csdl="mail";
-$con=mysql_connect("localhost","root","") or die(" khong the ket noi");
-mysql_select_db($csdl,$con);
-
-//$base_url='http://localhost:81/email_activation';
-?>
