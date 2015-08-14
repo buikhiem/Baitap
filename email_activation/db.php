29b873b9e897b3f5dce23b9ee15fc3142925363d
buikhiem <buikhiem1994@gmail.com>
2015-08-14 09:36:22 +0700

    noi dung moi

---
+++ email_activation/db.php
@@ -0,0 +1,7 @@
+<?php
+$csdl="mail";
+$con=mysql_connect("localhost","root","") or die(" khong the ket noi");
+mysql_select_db($csdl,$con);
+
+//$base_url='http://localhost:81/email_activation';
+?>
