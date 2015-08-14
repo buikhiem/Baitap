531cea3a279db3b5de8ce39933d3260e092bcb2e
buikhiem <buikhiem1994@gmail.com>
2015-08-14 09:15:59 +0700

    moi

--- Baitap/vtech_bai1/vtech_bai1/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$csdl="vtech1";
-$con=mysql_connect("localhost","root","") or die(" khong the ket noi");
-mysql_select_db($csdl,$con);
-
-?>
--- Baitap/vtech_bai1/vtech_bai1/logout.php
+++
@@ -1,9 +0,0 @@
- <meta charset="utf-8">
-<?php session_start();
-
-if (isset($_SESSION['username'])){
-	session_destroy();
-    unset($_SESSION['username']); // xóa session login
-}
-?>
-<a href="index.php">Đăng Nhập</a>
